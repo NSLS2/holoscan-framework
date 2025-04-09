549eb38c7f5fe86b05d5045e27027df1e8db095f
dleshchev <dleshchev@nvidia.com>
2025-04-09 12:49:01 -0400

    commented out thread pinning

--- eiger_dir/Holoptycho/ptycho_holo.py
+++ eiger_dir/Holoptycho/ptycho_holo.py
@@ -207,17 +207,17 @@ class PtychoApp(Application):
         self.add_flow(self.image_send,self.pty,{("frame_ready_num","frame_ready_num")})
         self.add_flow(self.point_proc,self.pty,{("pos_ready_num","pos_ready_num")})
 
-        pool1 = self.make_thread_pool("pool1", 1)
-        pool1.add(self.eiger_zmq_rx, True)
+        # pool1 = self.make_thread_pool("pool1", 1)
+        # pool1.add(self.eiger_zmq_rx, True)
     
-        pool2 = self.make_thread_pool("pool2", 7)
-        pool2.add(self.pos_rx, True)
-        pool2.add(self.image_batch, True)
-        pool2.add(self.image_proc, True)
-        pool2.add(self.image_send, True)
-        pool2.add(self.point_proc, True)
-        pool2.add(self.pty, True)
-        pool2.add(self.o, True)
+        # pool2 = self.make_thread_pool("pool2", 7)
+        # pool2.add(self.pos_rx, True)
+        # pool2.add(self.image_batch, True)
+        # pool2.add(self.image_proc, True)
+        # pool2.add(self.image_send, True)
+        # pool2.add(self.point_proc, True)
+        # pool2.add(self.pty, True)
+        # pool2.add(self.o, True)
         
 
         # self.add_flow(self.init_recon,self.pty_ctrl,{("init","ctrl_input")})
@@ -252,7 +252,7 @@ def main():
                 check_recession_period_ms=0.001,
                 stop_on_deadlock=True,
                 stop_on_deadlock_timeout=500,
-                strict_job_thread_pinning=True,
+                # strict_job_thread_pinning=True,
                 name="multithread_scheduler",
             )
     app.scheduler(scheduler)
