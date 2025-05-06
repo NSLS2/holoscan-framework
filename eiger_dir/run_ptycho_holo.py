02f487f2fd41dbe6bacdfe66f8254a679efeb0fd
dleshchev <dleshchev@nvidia.com>
2025-05-06 16:44:06 -0400

    help script to launch holo app with simulated stream

---
+++ eiger_dir/run_ptycho_holo.py
@@ -0,0 +1,8 @@
+import sys
+sys.path.append('/ptycho_gui/')
+
+# from nsls2ptycho.core.Holoptycho.datasource import main
+from nsls2ptycho.core.Holoptycho.ptycho_holo import main
+
+if __name__ == '__main__':
+    main()
